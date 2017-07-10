2ffbe4fbebe419f0c598abca79330b678e79ec26
hambardzumyan-mane <hambardzumyan.mane@gmail.com>
2017-07-10 23:40:23 +0400

    Make the selectors to start with different name

--- Intrusion-Detector-V2/src/app/home/home.component.ts
+++ Intrusion-Detector-V2/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', // Why do you name your slector "app-*"
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
